dd136d14a3de9b9271635fc398dca39ecd7e2c2e
sairam1318 <78812685+sairam1318@users.noreply.github.com>
2021-03-03 17:38:51 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,6 +1,6 @@
 import pyttsx3
 from playsound import playsound
-text = input("Enter a number: ")
+number = input("Enter a number: ")
 engine = pyttsx3.init()
-engine.say('Entered number is ' + text)
+engine.say('Entered number is ' + number)
 engine.runAndWait()
