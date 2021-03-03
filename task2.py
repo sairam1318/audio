e89b089129a98d1cd1c70f9a18231d4bfb3e5112
sairam1318 <78812685+sairam1318@users.noreply.github.com>
2021-03-03 17:12:13 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,3 +1,3 @@
 from playsound import playsound
-number = input("Enter a number: ")
-[playsound(x + '.mp3') for x in number]
+givenNumber = input("Enter a number: ")
+[playsound(x + '.mp3') for number in givenNumber]
